b14e5b248bc7d97c8beda100a5c92e7bbd08fef9
Abdelkhaleke01 <9024558@student.zadkine.nl>
2025-11-19 23:17:14 +0100

    klaar

--- src/Pages/ProjectDetails.jsx
+++ src/Pages/ProjectDetails.jsx
@@ -11,13 +11,13 @@ function ProjectDetails() {
         <p>
             Voor me eerste project moest ik een landen project maken wat voor een goed doel en ik had met me groepje het land suriname
          <br /><br />
-         Voor mijn andere project had ik met mijn groepje een escape room website gemaakt waar je puzzels moet oplossen om uiteindelijk de schat kist op het einde open te krijgen
+         Voor mijn tweede project had ik met mijn groepje een escape room website gemaakt waar je puzzels moet oplossen om uiteindelijk de schat kist op het einde open te krijgen
         </p>
       </div>
 
       <div className="details-images-row">
-        <img src="/img/zitten.jpg" alt="project preview" />
-        <img src="/img/zitten.jpg" alt="project preview 2" />
+        <img src="/img/Surinamepagina.png" alt="project preview" />
+        <img src="/img/schatproject.png" alt="project preview 2" />
       </div>
 
     </div>
